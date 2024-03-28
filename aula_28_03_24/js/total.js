2053aa51356031021abfa0e2c88ec9072732b041
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2024-03-28 19:25:47 -0300

    Faculdade

---
+++ aula_28_03_24/js/total.js
@@ -0,0 +1,6 @@
+var $input_quantidade = document.querySelector("#qtd");
+var $output_total = document.querySelector("#total");
+
+$input_quantidade.oninput = function() {
+    console.log('passei!')
+}
