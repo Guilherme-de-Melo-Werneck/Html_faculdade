cc0011eb81a374c22d2183aa67efe9a225152269
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2024-03-28 19:42:12 -0300

    Faculdade

--- aula_28_03_24/js/total.js
+++ aula_28_03_24/js/total.js
@@ -3,6 +3,17 @@ var $output_total = document.querySelector("#total");
 
 $input_quantidade.oninput = function() {
     var preco = document.querySelector("#preco").textContent;
-
+    preco = preco.replace("R$ ", "");
+    preco = preco.replace(",", ".");
+    console.log(typeof(preco))
+    preco = parseFloat(preco);
+    
     console.log(preco)
+    console.log(typeof(preco))
+
+    var quantidade = $input_quantidade.value;
+    var total = quantidade * preco;
+    total = "R$ " + total.toFixed(2);
+    total = total.replace(".", ",");
+    $output_total.value = total;
 }   
